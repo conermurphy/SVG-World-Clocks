27e2ffccb6c155a6e46af22fe9bb5ef45b21675b
Coner Murphy <conermurphy@gmail.com>
2019-10-03 21:52:09 +0100

    Redesigned choice clock

--- src/components/ChoiceSVGCircle.js
+++ src/components/ChoiceSVGCircle.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SVGCircleStyles from "./SVGCircleStyles.module.css";
 
-let cx = `37.5vw`;
-let cy = `37.5vh`;
-let sw = `2.25%`;
-let hourDA = Math.max(2 * Math.PI * (window.innerHeight * 0.3375), 1149.351672316);
-let minuteDA = Math.max(2 * Math.PI * (window.innerHeight * 0.315), 1191.763173139);
-let secondDA = Math.max(2 * Math.PI * (window.innerHeight * 0.2925), 848.230016469);
+let cx = `50`;
+let cy = `50`;
+let sw = `3px`;
+let hourDA = Math.max(2 * Math.PI * 40);
+let minuteDA = Math.max(2 * Math.PI * 37);
+let secondDA = Math.max(2 * Math.PI * 34);
 
 
 const ChoiceSVGCircle = ({ type, hour, minute, second }) => {
@@ -15,11 +15,11 @@ const ChoiceSVGCircle = ({ type, hour, minute, second }) => {
   const secondRadius = secondDA - ((secondDA/60) * second);
 
   if (type === "hour") {
-    return (<svg id="choiceHourCircle" className={SVGCircleStyles.choiceHourCircleSvg} style={{width: `75vw`, height: `75vh`}}><circle fill="none" stroke="#CE796B" strokeWidth={sw} cx={cx} cy={cy} r="33.75vh" style={{strokeDashoffset: hourRadius, strokeDasharray: hourDA}}/></svg>);
+    return (<svg id="choiceHourCircle" className={SVGCircleStyles.choiceHourCircleSvg} viewBox={`0 0 100 100`} style={{width: `750`, height: `750`}}><circle fill="none" stroke="#CE796B" strokeWidth={sw} cx={cx} cy={cy} r="40" style={{strokeDashoffset: hourRadius, strokeDasharray: hourDA}}/></svg>);
   } else if (type === "minute") {
-    return (<svg id="choiceMinuteCircle" className={SVGCircleStyles.choiceMinuteCircleSvg} style={{width: `75vw`, height: `75vh`}}><circle fill="none" stroke="#E7AD99" strokeWidth={sw} cx={cx} cy={cy} r="31.5vh" style={{strokeDashoffset: minuteRadius, strokeDasharray: minuteDA}}/></svg>);
+    return (<svg id="choiceMinuteCircle" className={SVGCircleStyles.choiceMinuteCircleSvg} viewBox={`0 0 100 100`} style={{width: `750`, height: `750`}}><circle fill="none" stroke="#E7AD99" strokeWidth={sw} cx={cx} cy={cy} r="37" style={{strokeDashoffset: minuteRadius, strokeDasharray: minuteDA}}/></svg>);
   } else {
-    return (<svg id="choiceSecondCircle" className={SVGCircleStyles.choiceSecondCircleSvg} style={{width: `75vw`, height: `75vh`}}><circle fill="none" stroke="#ECC8AF" strokeWidth={sw} cx={cx} cy={cy} r="29.25vh" style={{strokeDashoffset: secondRadius, strokeDasharray: secondDA}}/></svg>);
+    return (<svg id="choiceSecondCircle" className={SVGCircleStyles.choiceSecondCircleSvg} viewBox={`0 0 100 100`} style={{width: `750`, height: `750`}}><circle fill="none" stroke="#ECC8AF" strokeWidth={sw} cx={cx} cy={cy} r="34" style={{strokeDashoffset: secondRadius, strokeDasharray: secondDA}}/></svg>);
   }
 
 };
