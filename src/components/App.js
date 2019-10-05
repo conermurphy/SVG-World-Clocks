9516dae24643cebd9479b3f5fab4fdad9983a49e
Coner Murphy <conermurphy@gmail.com>
2019-10-05 09:44:22 +0100

    Responsiveness work

--- src/components/App.js
+++ src/components/App.js
@@ -30,16 +30,18 @@ class App extends React.Component {
           <div className={appStyles.worldClocks}>
             <h2>Times from around the world.</h2>
             <div className={appStyles.preClocksContainers}>
-              <Clock city={'Europe/London'} />
-              <Clock city={'America/New_York'} />
-              <Clock city={'America/Los_Angeles'} />
-              <Clock city={'Asia/Tokyo'} />
-              <Clock city={'Australia/Sydney'} />
-              <Clock city={'Pacific/Tahiti'}/>
-              <Clock city={'Europe/Paris'}/>
-              <Clock city={'Pacific/Auckland'}/>
-              <Clock city={'America/Sao_Paulo'}/>
-              <Clock city={'Europe/Moscow'}/>
+              <div className={appStyles.preClockContainerTop}>
+                <Clock city={'Europe/London'} />
+                <Clock city={'America/Los_Angeles'} />
+                <Clock city={'Asia/Tokyo'} />
+                <Clock city={'Australia/Sydney'} />
+              </div>
+              <div className={appStyles.preClockContainerBottom}>
+                <Clock city={'Pacific/Tahiti'}/>
+                <Clock city={'Pacific/Auckland'}/>
+                <Clock city={'America/Sao_Paulo'}/>
+                <Clock city={'Europe/Moscow'}/>
+              </div>
             </div>
           </div>
           <div className={appStyles.choiceSection}>
